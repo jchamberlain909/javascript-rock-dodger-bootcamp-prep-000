37e5de68292eb2ae6d4e1fc11c82725517d3b1da
Jared Chamberlain <jchamberlain909@gmail.com>
2018-04-26 15:10:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -145,6 +145,7 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+   e.stopPropagation();
    if(e.which===LEFT_ARROW){
      e.preventDefault();
      moveDodgerLeft();
