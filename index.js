f79eaead82d7b625ff45defecd37c0eaaf186c08
Jared Chamberlain <jchamberlain909@gmail.com>
2018-04-26 15:09:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -147,12 +147,10 @@ function moveDodger(e) {
    */
    if(e.which===LEFT_ARROW){
      e.preventDefault();
-     e.stopPropagration();
      moveDodgerLeft();
    }
    else if(e.which===RIGHT_ARROW) {
      e.preventDefault();
-     e.stopPropagration();
      moveDodgerRight();
    }
 
